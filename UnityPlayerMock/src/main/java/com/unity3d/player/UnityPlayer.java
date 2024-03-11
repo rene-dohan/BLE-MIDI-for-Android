d69bbffdd3b75b01e223324bedb1cb4ffae38320
Rene Dohan <dohan.rene@gmail.com>
2024-03-11 23:17:20 +0800

    Refactor midi bluetooth lib

--- UnityPlayerMock/src/main/java/com/unity3d/player/UnityPlayer.java
+++
@@ -1,9 +0,0 @@
-package com.unity3d.player;
-
-import android.app.Activity;
-
-public class UnityPlayer {
-    public static Activity currentActivity = null;
-    public static void UnitySendMessage(String gameObjectName, String methodName, String parameter) {
-    }
-}
--- UnityPlayerMock/src/main/java/com/unity3d/player/UnityPlayerActivity.java
+++
@@ -1,6 +0,0 @@
-package com.unity3d.player;
-
-import android.app.Activity;
-
-public class UnityPlayerActivity extends Activity {
-}
